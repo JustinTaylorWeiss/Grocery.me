01d38907b806a3039764302e99fc6e46a0d6e276
Justin Weiss <justintaylorweiss@gmail.com>
2021-02-23 03:48:22 -0500

    Added splash screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import hamburger from './assets/hamburger.svg'
+import hero from './assets/hero.jpg'
+import sampleLogo from './assets/sampleLogo.png'
 
 function App() {
-  return (
+
+  return <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Background"/>
+      <div className="navBar">
+        <img className="hamburgerMenu" src={hamburger} alt="Menu" height="50" width="50"/>
+        <div className="signUpLogInWrapper">
+          <button className="login"> <b> Log in </b> </button>
+          <button className="signUp"> <b> Sign up </b> </button>
+        </div>
+      </div>
+      <div className="ctaWrapper">
+        <img className="logoImage" src={sampleLogo} alt="Sample Logo"/>
+        <form action="" method="post">
+          <label for="CTA" className="ctaText">Find Food Near You</label><br/>
+          <input type="text" className="ctaInput" placeholder="Enter your street adress or zip code."/><br/>
+          <input type="submit" className="ctaSubmit" value="Find Food"/>
+        </form>
+      </div>
     </div>
-  );
+  </>;
 }
 
 export default App;

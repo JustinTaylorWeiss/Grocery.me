f5ee99808457f73052f530fa9a9d08d419588820
Justin Weiss <justintaylorweiss@gmail.com>
2021-03-02 11:09:34 -0500

    Restructured with styled types

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
+import React from 'react';
 import './App.css';
-import hamburger from './assets/hamburger.svg'
-import hero from './assets/hero.jpg'
-import sampleLogo from './assets/sampleLogo.png'
+
+import { SplashScreen } from './splashScreen';
+import { Shop } from './shop';
 
 function App() {
 
+  const [page, updatePage] = React.useState("Splash");
+
   return <>
     <div className="App">
-      <div className="Background"/>
-      <div className="navBar">
-        <img className="hamburgerMenu" src={hamburger} alt="Menu" height="50" width="50"/>
-        <div className="signUpLogInWrapper">
-          <button className="login"> <b> Log in </b> </button>
-          <button className="signUp"> <b> Sign up </b> </button>
-        </div>
-      </div>
-      <div className="ctaWrapper">
-        <img className="logoImage" src={sampleLogo} alt="Sample Logo"/>
-        <form action="" method="post">
-          <label for="CTA" className="ctaText">Find Food Near You</label><br/>
-          <input type="text" className="ctaInput" placeholder="Enter your street adress or zip code."/><br/>
-          <input type="submit" className="ctaSubmit" value="Find Food"/>
-        </form>
-      </div>
+      {
+        page === "Splash" ? <SplashScreen updatePage={updatePage}/> :
+        page === "Shop"   ? <Shop updatePage={updatePage}/>         :
+        <null/>
+      }
     </div>
   </>;
 }
